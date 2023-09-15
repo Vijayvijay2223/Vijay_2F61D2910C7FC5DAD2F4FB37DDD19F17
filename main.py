07f40ea43eb4d65cd67cff1e0ebbe77b59c1c23a
Vijayvijay2223 <144440168+Vijayvijay2223@users.noreply.github.com>
2023-09-15 10:51:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-def fact_rec(n):
-     if n==0 or n==1:
-        return 1
-     else:
-        return n*fact_rec(n-1)
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
 
-number = 2
-res = fact_rec(number)
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please deposit a positive amount.")
 
-print("The factorial of {} is {}.".format(number,res))
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Insufficient funds or invalid withdrawal amount.")
+
+    def display_balance(self):
+        print(f"Account balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+
+# Creating an instance of the BankAccount class
+account1 = BankAccount("12345", "John Doe", 1000.0)
+
+# Testing deposit and withdrawal functionality
+account1.display_balance()
+account1.deposit(500.0)
+account1.withdraw(200.0)
+account1.withdraw(1500.0)
